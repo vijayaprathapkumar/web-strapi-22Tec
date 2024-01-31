bcac56e64eba857e47b23fc643b132433b6f2cf3
vijayprathap <cube@genpixels.com>
2024-01-31 17:37:57 +0530

    graphql added

--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,3 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({});
+exports.default = {
+    //
+    graphql: {
+        config: {
+            endpoint: '/graphql',
+            shadowCRUD: true,
+            playgroundAlways: false,
+            depthLimit: 15,
+            amountLimit: 100,
+            apolloServer: {
+                tracing: false,
+            },
+        },
+    },
+};
